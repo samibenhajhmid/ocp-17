cefae3d71658bcbe5ca30a3e738aecb32acbda57
deep <deep@deeps-MacBook-Pro-2.local>
2023-12-29 08:32:12 +0100

    first commit

---
+++ src/main/java/com/ocp17/ocp17/Ocp17Application.java
@@ -0,0 +1,13 @@
+package com.ocp17.ocp17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ocp17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ocp17Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ocp17/ocp17/Ocp17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ocp17.ocp17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ocp17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
